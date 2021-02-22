a8a3400bb4f72df5e07c53b67d40fbcefbc96aa6
James Posey <jamesrposeyjr@gmail.com>
2021-02-22 14:09:55 -0600

    basic views for RN

--- App.js
+++ App.js
@@ -1,21 +1,43 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import {
+  StyleSheet,
+  Text,
+  View,
+  ScrollView,
+  Image,
+  TextInput,
+} from 'react-native';
 
 export default function App() {
+  const [text, setText] = useState('');
   return (
-    <View style={styles.container}>
+    <View style={{ padding: 10 }}>
+      <Text>Super Simple App</Text>
       <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <StatusBar style='auto' />
+      <Image
+        source={{
+          uri: 'https://reactnative.dev/docs/assets/p_cat2.png',
+        }}
+        style={{ width: 200, height: 200 }}
+      />
+      <TextInput
+        style={{
+          height: 40,
+          borderColor: 'gray',
+          borderWidth: 1,
+        }}
+        placeholder='Type here'
+        onChangeText={(text) => setText(text)}
+        defaultValue={text}
+      />
+      <Text>
+        {text
+          .split(' ')
+          .map((word) => word && 'null')
+          .join(' ')}
+      </Text>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
